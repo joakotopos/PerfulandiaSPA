4d2020964f2858403b5ed979da71f11f9a38a0fb
joakotopos <jo.sanhuezav@duocuc.cl>
2025-05-17 16:08:10 -0400

    Initial commit

---
+++ src/test/java/cl/duocuc/perfulandia/PerfulandiaSPA/PerfulandiaSpaApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.duocuc.perfulandia.PerfulandiaSPA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PerfulandiaSpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
