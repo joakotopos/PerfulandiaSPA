9bbba137ed752e33461c44ba6af3d3d767880ac5
joakotopos <jo.sanhuezav@duocuc.cl>
2025-05-22 23:12:54 -0400

    PedidoController
    se agrego pedidocontroller

---
+++ src/main/java/cl/duocuc/perfulandia/PerfulandiaSPA/controller/PedidoController.java
@@ -0,0 +1,47 @@
+package cl.duocuc.perfulandia.PerfulandiaSPA.controller;
+
+import cl.duocuc.perfulandia.PerfulandiaSPA.model.Pedido;
+import cl.duocuc.perfulandia.PerfulandiaSPA.service.PedidoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+@RestController
+@RequestMapping("/pedidos")
+public class PedidoController {
+
+    @Autowired
+    private PedidoService pedidoService;
+
+    @PostMapping("/cliente/{clienteId}")
+    public ResponseEntity<Pedido> crearPedido(@PathVariable Integer clienteId) {
+        return ResponseEntity.ok(pedidoService.crearPedido(clienteId));
+    }
+
+    @GetMapping("/cliente/{clienteId}")
+    public ResponseEntity<List<Pedido>> obtenerPedidosCliente(@PathVariable Integer clienteId) {
+        return ResponseEntity.ok(pedidoService.listarPedidosPorCliente(clienteId));
+    }
+
+    @GetMapping("/estado/{estado}")
+    public ResponseEntity<List<Pedido>> obtenerPedidosPorEstado(@PathVariable String estado) {
+        return ResponseEntity.ok(pedidoService.buscarPedidosPorEstado(estado));
+    }
+
+    @GetMapping("/rango-precio")
+    public ResponseEntity<List<Pedido>> obtenerPorRangoPrecio(
+            @RequestParam BigDecimal min,
+            @RequestParam BigDecimal max) {
+        return ResponseEntity.ok(pedidoService.buscarPedidosPorRangoPrecio(min, max));
+    }
+
+    @PutMapping("/{pedidoId}/estado/{nuevoEstado}")
+    public ResponseEntity<Pedido> actualizarEstado(
+            @PathVariable Integer pedidoId,
+            @PathVariable String nuevoEstado) {
+        return ResponseEntity.ok().build();
+    }
+}
